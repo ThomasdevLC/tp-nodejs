c740ff40e5a37ed9479ca5c086f4889c7b925f8d
ThomasdevLC <thomas.lecam@gmail.com>
2024-04-16 17:33:30 +0200

    new .env

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
