b78cb438f50ca28e69c4c9120c1c869ba0288535
ThomasdevLC <thomas.lecam@gmail.com>
2024-04-16 17:08:09 +0200

    mongodb database

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const gameRouter = require("./router/gameRouter");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
 });
 
 app.use("/game", gameRouter);
